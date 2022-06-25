93121370ca51daa6af46de00fcd4b5e8f99e90b1
Datnt <ntd4996@gmail.com>
2022-06-25 16:16:49 +0700

    feat:implement api

--- components/atoms/TabOverview.tsx
+++ components/atoms/TabOverview.tsx
@@ -1,102 +1,39 @@
 import Image from "next/image";
-import React from "react";
+import React, { FC } from "react";
 import styles from "./TabOverView.module.scss";
-
 export default function TabOverView(props: any) {
     const { data } = props;
+    console.log(data);
+
+    const RenderMetadata: FC = () => {
+        let renderObj;
+        if (data?.metadata) {
+            renderObj = Object.keys(data?.metadata).map((key) => {
+                return (
+                    <div className={styles.flexCol}>
+                        <div className={styles.title}>{key}</div>
+                        <div className={styles.contentMeta}>
+                            <span
+                                className={
+                                    data?.metadata[key] > 0
+                                        ? styles.textGreen
+                                        : styles.textRed
+                                }
+                            >
+                                {parseFloat(data?.metadata[key]).toFixed(2)}
+                            </span>
+                        </div>
+                    </div>
+                );
+            });
+        }
+        return <div className={styles.gird}>{renderObj}</div>;
+    };
 
     return (
         <div className="w-full container">
             <div className={styles.tradingView}>Tradingview</div>
-            <div className={styles.flexRow}>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>VỐN HÓA</div>
-                    <div>2.600 tỷ</div>
-                </div>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>SỐ LƯỢNG CP</div>
-                    <div>80.000.000</div>
-                </div>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>EPS</div>
-                    <div>7.014</div>
-                </div>
-            </div>
-
-            <div className={styles.flexRow}>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>BOOK VALUE</div>
-                    <div>15.182</div>
-                </div>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>P/E</div>
-                    <div>4.6</div>
-                </div>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>P/B</div>
-                    <div>21</div>
-                </div>
-            </div>
-
-            <div className={styles.flexRow}>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>ROE</div>
-                    <div className={styles.textGreen}>48.4%</div>
-                </div>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>ROA</div>
-                    <div className={styles.textGreen}>32.9%</div>
-                </div>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>TB KLGD</div>
-                    <div>292.210</div>
-                </div>
-            </div>
-
-            <div className={styles.flexRow}>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>TB GTGD</div>
-                    <div>9.5 tỷ</div>
-                </div>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>GDNN</div>
-                    <div className={styles.textGreen}>-1.3 tỷ</div>
-                </div>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>SỞ HỮU NN</div>
-                    <div className={styles.textGreen}>0.2%</div>
-                </div>
-            </div>
-
-            <div className={styles.flexRow}>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>NỢ/VCSH</div>
-                    <div className={styles.textGreen}>0.4</div>
-                </div>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>BIÊN LỢI NHUẬN</div>
-                    <div className={styles.textGreen}>51.0%</div>
-                </div>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>+/- 5 PHIÊN</div>
-                    <div className={styles.textGreen}>4.8%</div>
-                </div>
-            </div>
-
-            <div className={styles.flexRow}>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>+/- 20 PHIÊN</div>
-                    <div className={styles.textGreen}>15.182</div>
-                </div>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>+/- 60 PHIÊN</div>
-                    <div className={styles.textRed}>-0.16%</div>
-                </div>
-                <div className={styles.flexCol}>
-                    <div className={styles.title}>UPDATE BC</div>
-                    <div>Q4 2021</div>
-                </div>
-            </div>
+            <RenderMetadata />
 
             <div className={styles.titleContent}>Thông tin doanh nghiệp</div>
             <div className={styles.content}>
