66c31e9924fdefd57465e20c2fcb1c0e677743c5
datnt <ntd4996@gmail.com>
2022-07-24 00:40:20 +0700

    feat: add link to menu

---
+++ pages/exp/index.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import TabContainerExp from "../../components/atoms/TabContainerExp";
+import { currentLayoutSlice } from "../../stores/layout";
+
+export default function Exp() {
+    const Dispatch = useDispatch();
+
+    useEffect(() => {
+        changeLayoutState();
+    }, []);
+    const changeLayoutState = () => {
+        Dispatch(currentLayoutSlice.actions.updateIsShowNav(true));
+        Dispatch(currentLayoutSlice.actions.updateIsFixedHeader(true));
+        Dispatch(currentLayoutSlice.actions.updateIsShowHeader(true));
+        Dispatch(currentLayoutSlice.actions.updateIsBack(false));
+        Dispatch(currentLayoutSlice.actions.updateIsShowNav(false));
+    };
+    return (
+        <div className="pagePaddingTop">
+            <TabContainerExp />
+        </div>
+    );
+}
---
+++ pages/info/index.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { currentLayoutSlice } from "../../stores/layout";
+
+export default function Info() {
+    const Dispatch = useDispatch();
+
+    useEffect(() => {
+        changeLayoutState();
+    }, []);
+    const changeLayoutState = () => {
+        Dispatch(currentLayoutSlice.actions.updateIsShowNav(true));
+        Dispatch(currentLayoutSlice.actions.updateIsFixedHeader(true));
+        Dispatch(currentLayoutSlice.actions.updateIsShowHeader(true));
+        Dispatch(currentLayoutSlice.actions.updateIsBack(false));
+        Dispatch(currentLayoutSlice.actions.updateIsShowNav(false));
+    };
+    return (
+        <div className="pagePaddingTop">
+            <div className="pb-20 w-full">
+                <div className="textComingSoon">Coming soon...</div>
+            </div>
+        </div>
+    );
+}
